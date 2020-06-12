15d0c6531e0c78f1548cc99789e5d4046e97992f
shawn <shawnpatel196@gmail.com>
2020-06-12 12:11:44 -0500

    finished mvp plus names stretch task

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
+# from doublyLinkedList import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.size = 0
+        self.storage = []
+        self.order = []
 
     def append(self, item):
-        pass
+
+        if self.size < self.capacity:
+            self.order.append(item)
+            self.storage.append(item)
+            self.size +=1
+        else:
+            index = self.order.index(self.storage[0])
+            self.storage.pop(0)
+            self.order[index] = item
+            self.storage.append(item)
+                
 
     def get(self):
-        pass
+        return self.order
